09f57c30d21665c03670f229785963a4076d62b1
brentthayer1 <bthayer1@gmail.com>
2020-07-23 15:17:29 -0600

    updated doc strings

--- capstone1_funcs.py
+++ capstone1_funcs.py
@@ -8,115 +8,208 @@ def select_columns(df, columns):
     return df[columns]
 
 def drop_duplicates(df):
-    '''
+    """
     Drops any duplicate values in the dataframe
 
-    Input- df (pd.DataFrame)
-    Output- df (pd.DataFrame)
-    '''
+    Parameters:
+        df (DataFrame): DataFrame to drop duplicates from
+
+    Returns:
+        out (DataFrame)
+    """
+
     df.drop_duplicates(inplace=True)
+
     return df
 
+
 def time_ms_to_sec(df):
-    '''
-    Converts milliseconds to second.
+    """
+    Converts milliseconds to seconds
     Removes millisecond column and returns
     the dataframe with a new time_seconds
     column.
 
-    Input- df (pd.DataFrame)
-    Output- df (pd.DataFrame)
-    '''
+    Parameters:
+        df (DataFrame): DataFrame who's time
+        column will be converted
+
+    Returns:
+        out (DataFrame)
+    """
 
     df['time_seconds'] = df['duration_ms']//1000
     df.drop('duration_ms', axis=1, inplace=True)
     pop = df.pop('time_seconds')
     df.insert(3, 'time_seconds', pop)
 
+
 def drop_columns(df, cols):
+    """
+    Drops specified columns from DataFrame
+
+    Parameters:
+        df (DataFrame): DataFrame who's columns
+        will be dropped
+        cols (list): List of columns to be dropped
+
+    Returns:
+        out (DataFrame)
+    """
+
     df.drop(cols, axis=1, inplace=True)
 
-def check_for_nans(df, cols):
-    '''
-    Returns the number of NaNs for each specified column
-    in a dataframe
 
-    Inputs- df (pd.DataFrame), cols (list of columns)
-    Output- List of NaNs for each column
-    '''
+def check_for_nans(df, cols):
+    """
+    Checks for NaNs in specified columns
+
+    Parameters:
+        df (DataFrame): DataFrame to check for NaNs
+        cols (list): List of columns to check
+
+    Returns:
+        out (count): Number of NaNs for each specified column
+        in the dataframe
+    """
+
     for col in cols:
         print(f'NaNs in {col}:', df['acousticness'].isnull().sum())
 
 
-
 def plot_hist_and_means(ax, df_, column):
+    """
+    Plots the distributions and means for a
+    specified column in a DataFrame
+
+    Parameters:
+        ax (axes object): Axes to plot on
+        df_ (DataFrame): DataFrame who's column will be plotted
+        column (series): Columns to plot
+
+    Returns:
+        Histogram plot of specified column
+    """
+
     sns.distplot(df_[column], ax=ax,)
     ax.set_title(f'{column} mean: {round(df_[column].mean(), 2)}')
 
-def plot_hists_attribute(df_lst, column, color_list, df_name_lst):
-    for df_, colr, name in zip(df_lst, color_list, df_name_lst):
-        print(f'Mean value for {column} in {name}:', round(df_[column].mean(), 2))
-        sns.distplot(df_[column], color=colr)
-        plt.show()
 
-
-def cdf(value, array):
-    return (array<value).sum() / len(array)
-
-
-vcdf = np.vectorize(cdf, excluded = ['array'])
-
-
-def scatter_cdf(df_lst, column, color_list, df_name_lst):
-    for frame, clr, name in zip(df_lst, color_list, df_name_lst):
-        data = frame[column]
-        cdf_ = vcdf(value = data, array = data)
-        plt.scatter(data, cdf_, s=2, color=clr, label=name, marker='.')
-        plt.xlabel(f'{column}')
-        plt.legend()
-        plt.title(f'{column} vs. popularity')
-        print(f'{name} Mean:{np.mean(data)}. St. Dev:{np.std(data)}')
-
-def scatter_cdf_no_descrip(df_lst, column, color_list, df_name_lst):
-    for frame, clr, name in zip(df_lst, color_list, df_name_lst):
-        data = frame[column]
-        cdf_ = vcdf(value = data, array = data)
-        plt.scatter(data, cdf_, s=2, color=clr, label=name, marker='.')
-        plt.xlabel(f'{column}')
-        plt.legend()
-        plt.title(f'{column} vs. popularity')
-
+# def plot_hists_attribute(df_lst, column, color_list, df_name_lst):
+#     for df_, colr, name in zip(df_lst, color_list, df_name_lst):
+#         print(f'Mean value for {column} in {name}:', round(df_[column].mean(), 2))
+#         sns.distplot(df_[column], color=colr)
+#         plt.show()
 
 
 def plot_dists_pdf(ax, df_, column, low, high):
+    """
+    Plots the distribution for a specified column in a DataFrame
+
+    Parameters:
+        ax (axes object): axes to plot on
+        df_ (DataFrame): DataFrame who's column will be plotted
+        column (series): Column to plot
+        low (int/float): Lower x limit
+        high (int/float): Upper x limit
+
+    Returns:
+        PDF plot of the specified column
+    """
+
     sns.distplot(df_[column], hist=False, ax=ax)
     ax.set_xlim(low, high)
     ax.set_title(f'{column} pdf')
-    # ax.legend()
 
 
 def plot_dists_cdf(ax, df_, column, low, high):
+    """
+    Plots the distribution for a specified column in a DataFrame
+
+    Parameters:
+        ax (axes object): axes to plot on
+        df_ (DataFrame): DataFrame who's column will be plotted
+        column (series): Column to plot
+        low (int/float): Lower x limit
+        high (int/float): Upper x limit
+
+    Returns:
+        CDF plot of the specified column
+    """
+
     kwargs = {'cumulative': True}
     sns.distplot(df_[column], hist=False, hist_kws=kwargs, kde_kws=kwargs, ax=ax)
     ax.set_xlim(low, high)
     ax.set_title(f'{column} cdf')
-    # ax.legend()
 
 
 def plot_scatter(ax, df_, column, against, low, high):
+    """
+    Plots a scatter plot for a specified column in a DataFrame
+    against another column in the data frame.
+
+    Parameters:
+        ax (axes object): axes to plot on
+        df_ (DataFrame): DataFrame who's columns will be plotted
+        against each other
+        column (series): X Column to plot
+        against (series): Y Column to plot
+        low (int/float): Lower x limit
+        high (int/float): Upper x limit
+
+    Returns:
+        Scatter plot of specified columns
+    """
+
     ax.scatter(df_[column], df_[against], s=0.5, alpha=0.6)
     ax.set_xlim(low, high)
     ax.set_xlabel(column)
     ax.set_ylabel(against)
     ax.set_title(f'{column} vs. {against}')
 
+
 def dfs_info_list(df_lst, df_name_lst, column):
+    """
+    Returns the mean and standard deviation for each DataFrame
+    from a list of DataFrames.
+
+    Parameters:
+        df_lst (list): List of DataFrames to extract mean and
+        standard deviations from
+        df_name_lst (list): List of DataFrame names
+        column (series): The column of the specified data frame to
+        find the mean and standard deviation of
+
+    Returns:
+        (out) List of strings containing the mean and standard deviation
+        for each df
+    """
+
     lst = []
     for df_, df_name in zip(df_lst, df_name_lst):
         lst.append(f'{df_name}  Mean: {np.mean(df_[column]):0.03f}  Std Dev: {np.std(df_[column]):0.03f}')
+
     return lst
 
+
 def master_plotter(df_lst, df_name_lst, column, against, low, high, fs):
+    """
+    Plots the CDF, PDF and scatter plot for a specified column in a DataFrame
+
+    Parameters:
+        df_lst (list): List of DataFrames to plot together
+        df_name_lst (list): List of DataFrame names
+        column (series): Column to plot
+        against (series): Y Column to plot in scatter plot
+        low (int/float): Lower x limit
+        high (int/float): Upper x limit
+        fs (tuple-int/float): Figure size of output
+
+    Returns:
+        CDF, PDF, scatter plot and mean/ standard deviation for
+        the column in each DataFrame
+    """
+
     info = dfs_info_list(df_lst, df_name_lst, column)
     fig, ax = plt.subplots(2,2, figsize=fs)
     for df_ in df_lst:
@@ -129,25 +222,41 @@ def master_plotter(df_lst, df_name_lst, column, against, low, high, fs):
     fig.tight_layout()
 
 
-
-
-
-
-
 def spearman_correlation(frame, col1, col2):
+    """
+    Returns correlation and p-value for two series
+
+    Parameters:
+        frame (DataFrame): DataFrame who's columns will be compared
+        col1 (series): First Series being compared
+        col2 (series): Second Series being compared
+
+    Returns:
+        Correlation between the two series and the p-value
+    """
+
     a, b = frame[col1], frame[col2]
     correlation, pvalue = stats.spearmanr(a, b)
     print(f'{col1} and {col2} have a correlation of {round(correlation, 3)}, with a p-value of {pvalue:.3e}') 
 
+
 def plot_correlations(df_, columns):
+    """
+    Plots the correlation between specified columns
+
+    Parameters:
+        df_ (DataFrame): DataFrame who's columns will be plotted
+        columns (list): The columns of the specified DataFrame to plot
+
+    Returns:
+        Correlation heat map
+    """
+
     Var_Corr = df_[columns].corr()
     sns.heatmap(Var_Corr, xticklabels=Var_Corr.columns, yticklabels=Var_Corr.columns, annot=True)
 
+
+
+
 if __name__ == '__main__':
-
-    file_path = '/Volumes/b/Galvanize/DS-RFT4/capstones-RFT4/datasets/music2/test1.csv'
-
-    df = pd.read_csv(file_path)
-    cols = ['artist_name', 'popularity', 'tempo']
-    
-    print(scatter_plot(df, df['popularity'], df['tempo']))
+    pass
